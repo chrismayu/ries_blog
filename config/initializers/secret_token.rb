9e594752c624f4ee647106306271cb5a92106b2a
Chris Mailloux <chrismayu@gmail.com>
2013-02-07 20:00:06 -0500

    changed login and sercet token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RiesBlog::Application.config.secret_token = 'f594db84aa1703d5e356061d9ade57a8b1e5a444b4c7a101a819ba8da4f0b153e5035ee5ce9575b3c83c25f8fc29da91d90b6c07cb8a2de9a534a105556b0258'
+RiesBlog::Application.config.secret_token = 'f594db84aa1703d5e356061d9ade57a8b1ehsjaskh9934fjfj394hf5a444b4c7a101a819ba8da4f0b153e5035ee5ce9575b3c83c25f8fc29da91d90b6c07cb8a2de9a534a105556b0258'
