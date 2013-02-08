adc53b55bdd85e9067cb4a3ad37e05ac915c9897
Chris Mailloux <chrismayu@gmail.com>
2013-02-07 21:36:14 -0500

    up to "prior to a post being saved"
    http://www.logansbailey.com/2011/02/27/adding-authorization-using-devise
    /

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130208003923) do
+ActiveRecord::Schema.define(:version => 20130208023031) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
